2aaa378eeb95cac176b96a8b9b4be5916cdf1fa2
guanlin.shen <guanlin.shen@lachesis-mh.com>
2020-03-29 18:41:03 +0800

    增加数据示例

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import request from 'umi-request';
 import { Button } from 'antd';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function fetchUsers() {
+      setUsers(await request.get('/api/users'));
+    }
+
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <Button>从这里开始</Button>
+        <h1>用户列表</h1>
+        <pre style={{ textAlign: 'left' }}>
+          {JSON.stringify(users, null, 2)}
+        </pre>
+      <Button>开始吧</Button>
     </div>
   );
 }
