97d9e2305854d1752b23ff8aa0c849632d4119bc
guanlin.shen <guanlin.shen@lachesis-mh.com>
2020-03-29 18:15:45 +0800

    增加描述

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://10.2.3.103:55555',
+            changeOrigin: true,
+        })
+    );
+};
