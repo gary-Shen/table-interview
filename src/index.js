8b255f7fe13c1b35399838a3a2a037da76e7fc8d
guanlin.shen <guanlin.shen@lachesis-mh.com>
2020-03-30 10:12:34 +0800

    添加路由

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename="/">
+        <Switch>
+          <Route path="/">
+            <App />
+          </Route>
+        </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
